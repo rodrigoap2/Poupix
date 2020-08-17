6a49dcfe00957eb69f5ff929c06e7343e8b92334
rodrigoap2 <rap2@cin.ufpe.br>
2020-08-17 15:27:00 -0300

    Create payment method choice screen

--- PoupixTerminal/app/src/main/java/com/example/poupixterminal/MainActivity.java
+++ PoupixTerminal/app/src/main/java/com/example/poupixterminal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.poupixterminal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
---
+++ PoupixTerminal/app/src/main/java/com/example/poupixterminal/PaymentMethodChoice.java
@@ -0,0 +1,14 @@
+package com.example.poupixterminal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentMethodChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_method_choice);
+    }
+}
