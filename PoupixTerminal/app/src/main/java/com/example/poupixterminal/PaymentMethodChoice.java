7bc1c27156c41e5036e047f3426dd401453d3afe
rodrigoap2 <rap2@cin.ufpe.br>
2020-08-17 17:08:07 -0300

    Parceling logic creation and integration with method choice screen

--- PoupixTerminal/app/src/main/java/com/example/poupixterminal/PaymentMethodChoice.java
+++ PoupixTerminal/app/src/main/java/com/example/poupixterminal/PaymentMethodChoice.java
@@ -1,6 +1,7 @@
 package com.example.poupixterminal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,20 +10,34 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class PaymentMethodChoice extends AppCompatActivity {
-    PaymentInformations paymentInformations;
-    public void definePaymentMethod(View view){
-        Button buttonChosen = (Button) view;
-        String methodChosen = buttonChosen.getTag().toString();
-        paymentInformations.setPaymentMethod(methodChosen);
-    }
+    public PaymentInformations paymentInformations;
+
+    private View.OnClickListener choosePaymentMethod = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button buttonChosen = (Button) view;
+            buttonChosen.setOnClickListener(this);
+            String methodChosen = buttonChosen.getTag().toString();
+            paymentInformations.setPaymentMethod(methodChosen);
+            Intent intent = new Intent(PaymentMethodChoice.this, PaymentParcelling.class);
+            intent.putExtra("PaymentInfo",paymentInformations);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method_choice);
+        setTitle("");
         Intent intent = getIntent();
         paymentInformations = intent.getParcelableExtra("PaymentInfo");
         TextView textView = (TextView) findViewById(R.id.transactionValueText);
         textView.setText(paymentInformations.getValue()); //Setting the text to show the value chosen in the value assign screen
+        ConstraintLayout methodLayout = (ConstraintLayout) findViewById(R.id.methodConstraint);
+        for(int i = 0; i < methodLayout.getChildCount(); i++){
+            Button button = (Button) methodLayout.getChildAt(i);
+            button.setOnClickListener(choosePaymentMethod);
+        }
     }
 }
--- PoupixTerminal/app/src/main/java/com/example/poupixterminal/PaymentParcelling.java
+++ PoupixTerminal/app/src/main/java/com/example/poupixterminal/PaymentParcelling.java
@@ -2,13 +2,22 @@ package com.example.poupixterminal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class PaymentParcelling extends AppCompatActivity {
+    private PaymentInformations paymentInformations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_parcelling);
+        setTitle("");
+        Intent intent = getIntent();
+        this.paymentInformations = intent.getParcelableExtra("PaymentInfo");
+        TextView textView = (TextView) findViewById(R.id.transactionValueText);
+        textView.setText(this.paymentInformations.getValue()); //Setting the text to show the value chosen in the value assign screen
     }
 }
