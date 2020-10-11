0b2fe3f413dfb3f705408bf436aa6aab205775aa
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-10-10 23:41:52 -0300

    Fix get nearby stores

--- api/src/main/java/poupix/store/transport/http/response/StoreResponse.java
+++ api/src/main/java/poupix/store/transport/http/response/StoreResponse.java
@@ -1,7 +1,5 @@
 package poupix.store.transport.http.response;
 
-import java.util.List;
-import java.util.Map;
 import lombok.Builder;
 import lombok.Value;
 import poupix.store.dal.model.Coordinates;
@@ -12,9 +10,9 @@ public class StoreResponse {
   String id;
   String name;
   String description;
-  String category;
+  String type;
   String address;
   Coordinates coordinates;
-  List<String> pictures;
-  Map<String, Double> cashback;
+  Object pictures;
+  Object cashback;
 }
